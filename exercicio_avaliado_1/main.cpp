af7245604b4a9be346f6aac4f52e1476e4535045
coutjj <coutinholima.j@poli.ufrj.br>
2021-04-15 12:12:16 -0300

    Alterando numero da turma

--- exercicio_avaliado_1/main.cpp
+++ exercicio_avaliado_1/main.cpp
@@ -3,7 +3,7 @@
   *  Universidade Federal do Rio de Janeiro
   *  Escola Politecnica
   *  Departamento de Eletronica e de Computacao
-  *  EEL770 - Linguagens de Programação - Turma 2020/2
+  *  EEL670 - Linguagens de Programação - Turma 2020/2
   *  Prof: Miguel Elias Mitre Campista
   *  Autor: Juan Coutinho Lima
   * 
